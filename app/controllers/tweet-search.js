b8b80fb2f9067123a459b2721b324edba24f623e
Kevin Gomez <kkristoffergomez@gmail.com>
2018-02-23 16:52:01 -0600

    It works

--- app/controllers/tweet-search.js
+++ app/controllers/tweet-search.js
@@ -2,5 +2,12 @@ import Controller from '@ember/controller';
 import { computed } from "@ember/object";
 
 export default Controller.extend({
-  tweets: computed.alias("model.tweet")
+  tweets: computed.alias("model.tweet"),
+  displayTweets: true,
+  actions: {
+    toggleDisplay() {
+      console.log("Hello!");
+      this.toggleProperty('displayTweets');
+    }
+  }
 });
